f68b4a478b626d0241b28b9270fcc80aa142ae2f
GaryGSC <GaryGSC@users.noreply.github.com>
2021-05-13 20:27:45 -0600

    Automatically generate API docs
    This plugin uses our registered routes and the schemas we've defined
    to automatically generate OpenAPI documentation in JSON or YAML. It can
    serve that directly, as well as a human-friendly UI.

---
+++ backend/app/src/plugins/openapi.js
@@ -0,0 +1,19 @@
+'use strict'
+const fp = require("fastify-plugin")
+
+// This provides /documentation (HTML), /documentation/json, and /documentation/yaml
+module.exports = fp(async function (fastify, opts) {
+  fastify.register(require('fastify-swagger'), {
+    routePrefix: '/documentation',
+    openapi: {
+      info: {
+        title: 'Ufinance',
+        description: 'Ufinance API',
+        version: '0.1.0'
+      },
+      servers: [{
+        url: 'http://localhost:8080' // TODO: Production URL
+      }],
+    }
+  })
+})
