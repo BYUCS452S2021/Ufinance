3a87fbcfb3e64af5c5027594b064dcd8ed982175
GaryGSC <GaryGSC@users.noreply.github.com>
2021-05-13 20:25:14 -0600

    Add schemas to routes
    This improves performance and allows tooling to handle things like
    validating input or generating documentation.

--- backend/app/src/routes/health.js
+++ backend/app/src/routes/health.js
@@ -4,6 +4,19 @@ module.exports = async function (fastify, opts) {
   fastify.route({
     method: 'GET',
     url: '/health',
+    schema: {
+      description: 'Healthcheck',
+      summary: 'Healthcheck',
+      tags: ['Health'],
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            status: { type: 'string' }
+          }
+        }
+      }
+    },
     handler: async (request, reply) => ({ status: 'pass' })
   })
 }
--- backend/app/src/routes/time.js
+++ backend/app/src/routes/time.js
@@ -4,6 +4,18 @@ module.exports = async function (fastify, opts) {
   fastify.route({
     method: 'GET',
     url: '/time',
+    schema: {
+      description: 'Get time',
+      tags: ['Time'],
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            time: { type: 'string' }
+          }
+        }
+      }
+    },
     handler: async (request, reply) => {
       const { rows: [{ now: time }] } = await fastify.pg.query('SELECT NOW()')
       return { time }
