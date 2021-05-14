4f55706289a433da39dd40ae9ce93d112770d3c3
Kenneth Vargas <55116120+kenneth-vargas@users.noreply.github.com>
2021-05-13 22:04:33 -0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -35,3 +35,27 @@ ALTER TABLE "strategies" ADD FOREIGN KEY ("investment_strategy") REFERENCES "use
 
 ALTER TABLE "stock_prices" ADD FOREIGN KEY ("stock_ticker") REFERENCES "holdings" ("stock_ticker");
 
+
+/*
+The team got together and designed this database. 
+users(user_id, email, password_hash, first_name, middle_name, last_name, investment_strategy)
+	Foreign key: investment_strategy references strategies 
+The users table will hold all the information of our clients. The user_id is an auto generated number that will work as the primary key of the table. The investment strategy is a foreing key that references the strategies table which holds the client’s investment preference. This table represents an user. 
+
+stock_prices(stock_ticker, timestamp, price, score) 
+	Foreign key: stock_ticker references holdings
+The stock prices table holds the closing price of the stocks that we use in our portfolios. The ticker and timestamp act as a joint primary key. This table will be used to calculate scores for the stocks and use them in portfolios depending on the client's investment strategy. This table represents a stock. 
+
+strategies(investment_strategy, upper_bound, lower_bound)
+The strategie stable holds an upper bound and lower bound of the assets that can be included in portfolios with the given investment strategy. 
+
+holdings(user_id, stock_ticker, number_of_shares)
+	Foreign key: user_id references users
+	Foreign key: stock_ticker references stock_prices
+The holdings table holds the current securities that are part of the clients portafolios. This table represents portfolios. 
+
+No columns contain more than one value. Each relationship depends on the key(s) of the table.
+The names of the columns of the table are pretty descriptive. Most of the logic behind the trades will come from scripts running in the background.  
+*/
+
+
